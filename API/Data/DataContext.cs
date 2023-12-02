fb5565fe4e933981d52dc3a4e07763e8ee5427d1
Rusty Divine <rdivine@northwell.edu>
2023-12-02 16:15:59 -0600

    Setting up sqlite and migration

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    // Add your entity DbSet properties here
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure your entity configurations here
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public DbSet<AppUser> Users { get; set; }
+    
+    
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,17 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
