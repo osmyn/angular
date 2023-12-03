5974fd6a532d6c1c9881f4569111169214d17a39
Rusty Divine <rdivine@northwell.edu>
2023-12-03 15:55:33 -0600

    First api call from client

--- API/Program.cs
+++ API/Program.cs
@@ -6,15 +6,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.UseAuthorization();
 
